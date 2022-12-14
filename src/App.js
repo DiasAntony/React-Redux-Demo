6f4b66e8b1f993443460c68a794649f17a2046a0
diaz <diazantony9997@example.com>
2022-12-14 10:31:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import UserProfile from './components/UserProfile'
+import Auth from "./components/Auth";
+
+
+
+
+function App() {
+
+  const isAuthenticated=useSelector((state)=>state.auth.isAuthenticated);
+
+  return (
+    <Fragment>
+      <Header />
+{isAuthenticated ? <UserProfile />:<Auth />} 
+    {isAuthenticated && <Counter />}
+    </Fragment>
+  );
+}
+
+export default App;
